8d05fa29469175fd41436119a7502e65531ec2af
Sion Smith <sion.smith@gmail.com>
2021-04-14 16:33:42 +0100

    inital commit

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "table" {
+  type = map(any)
+}
+
+variable "point_in_time_recovery" {
+  type    = bool
+  default = true
+}
+
+variable "ttl_enabled" {
+  default = false
+}
+
+variable "server-side-encryption" {
+  type    = bool
+  default = true
+}
+
+variable "common_tags" {
+  description = "This is to help you add tags to your cloud objects"
+  type        = map(any)
+}
